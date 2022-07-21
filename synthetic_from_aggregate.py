64fcc445158010fbe74f5a138490798be532db50
Jack Bandy <jackbandy@u.northwestern.edu>
2022-07-21 13:01:54 -0500

    First step at creating synthetic data

---
+++ synthetic_from_aggregate.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# read in aggregate data
+df_hits = pd.read_csv('data/q2/impressions-top-1000-domains.csv')
+df_users = pd.read_csv('data/q2/unique_users-top-1000-domains.csv')
+df = df_users.merge(df_hits,on='url_domain')
+
+# citizen browser had 1,917 panelists as of December 2020
+user_ids = pd.Series(list(range(1,1919)))
+
+domain_visitors = []
+for index, row in df.iterrows():
+    users_df = pd.DataFrame(user_ids.sample(row['unique_users']))
+    users_df.columns = ['user_id']
+    users_df['url_domain'] = row['url_domain']
+    domain_visitors.append(users_df)
+
+#TODO set up random date
+
+# save output
+synthetic = pd.concat(domain_visitors)
+synthetic.to_csv('synthetic_visitors/visitors.csv',index=False)
